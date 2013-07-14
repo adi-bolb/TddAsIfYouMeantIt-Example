3657cec07ed59e8634ae6e72415f2336132811d7
Adi Bolboaca <adi_bolb123@yahoo.com>
2013-07-14 14:21:25 +0300

    Added references to local junit and hamcrest jars

--- java/TicTacToe/src/ro/adrianbolboaca/tictactoe/TemporaryTestClass.java
+++ java/TicTacToe/src/ro/adrianbolboaca/tictactoe/TemporaryTestClass.java
@@ -1,5 +1,11 @@
 package ro.adrianbolboaca.tictactoe;
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class TemporaryTestClass {
 
+	@Test
+	public void testname() throws Exception {
+
+	}
 }
